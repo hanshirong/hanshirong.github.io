243e85119df08053db1a7bcaf50b8fa74990a865
jiuge <1418264042@qq.com>
2022-04-19 20:15:22 +0800

    上传三天的

---
+++ js30/06/index.js
@@ -0,0 +1,45 @@
+// 获取到搜索值
+const searchInput = document.querySelector('.searchBox');
+const suggestions = document.querySelector('.suggestions');
+// 数据接口
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => cities.push(...data));
+console.log(cities);
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    // here we need to figure out if the city or state matches what was searched
+    // /i忽略大小写 /g全局搜索
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex)
+  });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+    return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+}
+
+
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
---
+++ js30/08/index.js
@@ -0,0 +1,68 @@
+
+// 颜色，画笔粗细，橡皮擦的初始值
+var color = "#66ccff"
+var size = "1"
+var eraser = false
+var isDrawing = false
+
+const inputs = document.querySelectorAll("input");
+
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext('2d');
+// 图形轮廓的颜色
+ctx.strokeStyle = color;
+// 线条宽度
+ctx.lineWidth = size;
+// 线条的末端以圆形结束
+ctx.lineCap = "round";
+// 两条线段的连接处以圆形连接
+ctx.lineJoin = "round";
+// 初始点是0,0点
+lastX = 0;
+lastY = 0;
+// 更新画笔参数
+function updateValue(){
+    if(this.name == "size"){
+        ctx.lineWidth = this.value;
+    }
+    if(this.checked){
+        ctx.strokeStyle = "white";
+    }else {
+        // 获取当前的颜色值
+        ctx.strokeStyle = document.getElementById("color").value;
+    }
+}
+
+function draw(e){
+    if(!isDrawing)
+        return;
+    if(e.type =="mousemove"){
+        x = e.offsetX;
+		y = e.offsetY;
+    }
+    ctx.beginPath();
+    ctx.moveTo(lastX,lastY);
+    ctx.lineTo(x,y);
+    ctx.stroke();
+    [lastX,lastY] = [x,y]
+}
+// 监听参数的改变
+inputs.forEach(input => input.addEventListener("change",updateValue))
+
+// 鼠标按下
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+// 鼠标移动
+canvas.addEventListener('mousemove', draw);
+// 鼠标松开
+canvas.addEventListener('mouseup', () => isDrawing = false);
+// 鼠标一旦离开canvas容器，必须将draw函数给清理掉
+canvas.addEventListener('mouseout', () => isDrawing = false);
+
+// 清空当前画布
+var btn = document.getElementById("clear");
+btn.onclick=function(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
